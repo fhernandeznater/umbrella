f2f839912a533e505477202d932acf4283aa0df9
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-01-03 16:30:33 +0000

    Cleaned up require statements

--- umbrella.rb
+++ umbrella.rb
@@ -12,8 +12,6 @@ pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_
 # Place a GET request to the URL
 raw_response = HTTP.get(pirate_weather_url)
 
-require "json"
-
 parsed_response = JSON.parse(raw_response)
 
 currently_hash = parsed_response.fetch("currently")
